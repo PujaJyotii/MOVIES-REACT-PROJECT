399701e4fbc0d4516406efbf16cc247b134401f1
PujaJyotii <pujajyoti111>
2023-09-30 18:27:26 +0530

    PostAndGet

--- src/App.js
+++ src/App.js
@@ -15,21 +15,27 @@ const [error,setError] = useState(null)
     setIsLoading(true)
     setError(null)
     try {
-   const response= await fetch('https://swapi.dev/api/films/');
+   const response= await fetch('https://react-http-9877a-default-rtdb.firebaseio.com/movies.json');
     if(!response.ok)
       {
         throw new Error('something went wrong!')
       }
       const data= await response.json()
-      const transformedMovies = data.results.map(movieData => {
-        return {
-          id:movieData.episode_id,
-          title:movieData.title,
-          openingText:movieData.opening_crawl,
-          releaseData:movieData.release_date
-        }
-      })
-      setMovies(transformedMovies)
+      
+      const loadedMovies =[];
+      for(const key in data)
+      {
+        loadedMovies.push
+        (
+          {
+            id:key,
+            title:data[key].title,
+            openingText: data[key].openingText,
+            releaseDate: data[key].releaseDate,
+          }
+        )
+      }
+      setMovies(loadedMovies)
      
     
    }
@@ -43,8 +49,17 @@ useEffect(() => {
   fetchMovieHandler()
 },[fetchMovieHandler])
 
-const onAddMovieHandler = (movie) => {
-  console.log(movie)
+const onAddMovieHandler = async (movie) => {
+ const response = await fetch('https://react-http-9877a-default-rtdb.firebaseio.com/movies.json',{
+    method:'POST',
+    body: JSON.stringify(movie),
+    headers:{
+      'Content-Type':'application/json'
+    }
+
+  })
+  const data = await response.json()
+  console.log(data)
 }
 
   let content = <p>Found no movies</p>
